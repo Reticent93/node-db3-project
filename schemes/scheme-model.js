3cdd899f40ad668f4cdf325b24342073f4f72318
Greg <remph819@gmail.com>
2020-03-03 21:00:19 -0700

    added findByStepsId function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,22 @@
 const db = require('../data/config');
 
 module.exports = {
-	find
+	find,
+	findById,
+	findSteps
 };
 
 function find() {
 	return db('schemes');
+}
+
+function findById(id) {
+	return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+	return db('steps as s')
+		.join('schemes', 'schemes.id', 's.scheme_id')
+		.where('s.scheme_id', id)
+		.select('s.id', 'step_number', 'schemes.scheme_name');
 }

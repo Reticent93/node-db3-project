82963bda2fe773a1715e1e94fc6150a105006b8b
Greg <remph819@gmail.com>
2020-03-03 18:57:11 -0700

    added find function

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const server = require('./server.js');
+const express = require('express');
+const helmet = require('helmet');
+const schemeRouter = require('./schemes/scheme-router');
 
+// const server = express();
 const PORT = process.env.PORT || 5000;
 
+server.use(helmet());
+server.use(express.json());
+server.use('/schemes', schemeRouter);
+
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+	console.log(`Listening on port ${PORT}...`);
 });
